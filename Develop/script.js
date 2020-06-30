1d0b7eb3dae16ff65d919759d1dfab818deb6596
Matthew Kerr <matt777k@gmail.com>
2020-06-30 18:08:36 -0400

    lowercase is not yet working but made some progres

--- Develop/script.js
+++ Develop/script.js
@@ -1,88 +1,129 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 //Create a series of prompts with password criteria
 //Length of password between 8 and 128 characters
-//Lowercase
+//Lowercase, while loops?
 //Uppercase
 //Numbers
 //Special characters
 //Store selections for password generations
-  //create one master array of choices
+//create one master array of choices using .push
 //Password matches selection
 //Password is generated - function
-  //Loop is how many characters user selected
-    //math.random and get a character
+//Loop is how many characters user selected
+//math.random and get a character
 //Password is displayed in alert or presented in the box
+
+var arrayOfArrays = "";
 
 var length = prompt("Choose a password length between 8 and 128 characters");
 
-  if (length < 8 || length > 128) {
-    alert("Password length has to be between 8 and 128 characters!");
-    location.reload()
+if (length < 8 || length > 128) {
+  alert("Password length has to be between 8 and 128 characters!");
+  location.reload();
+}
+
+  var charLowerCase = confirm("Include lowercase letters?");
+
+  var lowercaseLetters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  //If Lowercase confirmed. Generate random lowercase letters and add them to arrayOfArrays
+  if (charLowerCase) {
+    alert("Lowercase confirmed");
+    for (var i = 0; i < length; i++) {
+      console.log(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]);
+      console.log(lowercaseLetters.concat(arrayOfArrays));
+    }
+    
   }
 
-var charLowerCase = confirm("Include lowercase letters?");
 
-var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var charUpperCase = confirm("Include uppercase letters?");
 
-for(var i=0; i < lowercaseLetters.length; i++) {
-  console.log(index.of(Math.random[i]))
-}
+  var uppercaseLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
-if (charLowerCase) {
-  alert("Lowercase confirmed")
-}
-else {
-  alert("Lowercase declined")
-}
+  if (charUpperCase) {
+    alert("Uppercase confirmed");
+  }
 
-var charUpperCase = confirm("Include uppercase letters?");
+  var charNumeric = confirm("Include numbers?");
 
-var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-if (charUpperCase) {
-  alert("Uppercase confirmed")
-}
-else {
-  alert("Uppercase declined")
-}
+  if (charNumeric) {
+    alert("Numbers Confirmed");
+  }
 
-var charNumeric = confirm("Include numbers?");
+  var charSpecial = confirm("Include Special Characters?");
 
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
 
-if (charNumeric) {
-  alert("Numbers Confirmed")
-}
-else {
-  alert("Numbers Declined")
-}
-
-var charSpecial = confirm("Include Special Characters?");
-
-var specialChar = ["!","@","#","$","%","^","&","*","?"];
-
-if (charSpecial) {
-  alert("Special Characters Confirmed")
-}
-else {
-  alert("Special Characters Declined")
-}
-
-
+  if (charSpecial) {
+    alert("Special Characters Confirmed");
+  }
