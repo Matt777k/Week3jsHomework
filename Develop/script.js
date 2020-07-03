8a1056ffda42f11efc004ce488d9f2de01c8dd46
Matthew Kerr <matt777k@gmail.com>
2020-07-03 18:53:16 -0400

    after talking with danny. organizing some codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write finalPassword to the #finalPassword input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var finalPassword = generatePassword();
+  var passwordText = document.querySelector("#finalPassword");
 
-  passwordText.value = password;
+  passwordText.value = finalPassword;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Create a series of prompts with password criteria
-//Length of password between 8 and 128 characters
+//Create a series of prompts with finalPassword criteria
+//Length of finalPassword between 8 and 128 characters
 //Lowercase, while loops?
 //Uppercase
 //Numbers
 //Special characters
-//Store selections for password generations
+//Store selections for finalPassword generations
 //create one master array of choices using .push
 //Password matches selection
 //Password is generated - function
@@ -28,7 +28,72 @@ generateBtn.addEventListener("click", writePassword);
 
 var arrayOfArrays = [];
 
-var length = prompt("Choose a password length between 8 and 128 characters");
+var lowercaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var uppercaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+
+//choose length
+var length = prompt("Choose a finalPassword length between 8 and 128 characters");
+
+//Selection of desired characters
 
 if (length < 8 || length > 128) {
   alert("Password length has to be between 8 and 128 characters!");
@@ -37,76 +102,19 @@ if (length < 8 || length > 128) {
 
   var charLowerCase = confirm("Include lowercase letters?");
 
-  var lowercaseLetters = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
+ 
 
   //If Lowercase confirmed. Generate random lowercase letters and add them to arrayOfArrays
   if (charLowerCase) {
     alert("Lowercase confirmed");
-    for (var i = 0; i < length; i++) {
-      var resultLower = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
-      arrayOfArrays.push(resultLower);
+      arrayOfArrays.push(lowercaseLetters);
     }
-    console.log(arrayOfArrays);
-  }
+  
 
 
   var charUpperCase = confirm("Include uppercase letters?");
 
-  var uppercaseLetters = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
+  
 
   if (charUpperCase) {
     alert("Uppercase confirmed");
@@ -114,12 +122,11 @@ if (length < 8 || length > 128) {
       var resultUpper = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
       arrayOfArrays.push(resultUpper);
     }
-    console.log(arrayOfArrays);
   }
 
   var charNumeric = confirm("Include numbers?");
 
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+ 
 
   if (charNumeric) {
     alert("Numbers confirmed");
@@ -127,12 +134,11 @@ if (length < 8 || length > 128) {
       var resultNum = numbers[Math.floor(Math.random() * numbers.length)];
       arrayOfArrays.push(resultNum);
     }
-    console.log(arrayOfArrays);
   }
 
   var charSpecial = confirm("Include Special Characters?");
 
-  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+ 
 
   if (charSpecial) {
     alert("Special characters confirmed");
@@ -140,13 +146,61 @@ if (length < 8 || length > 128) {
       var resultSpecial = specialChar[Math.floor(Math.random() * specialChar.length)];
       arrayOfArrays.push(resultSpecial);
     }
-    console.log(arrayOfArrays);
   }
 
-  function arrayBalance(equalize) {
-
-  arrayOfArrays = length
-  }
-
-  arrayBalance();
   console.log(arrayOfArrays);
+
+
+
+
+
+
+
+
+  if (arrayOfArrays === length) {
+    console.log(finalPassword);
+  }
+  else {
+    finalPassword = arrayOfArrays.concat()
+    finalPassword.length = length;
+  }
+
+  console.log(finalPassword);
+  //set arrayOfArrays length equal to length 
+
+  var options = [lowercaseLetters, uppercaseLetters, numbers, specialChar]
+
+  checkPassword();
+
+    function checkPassword() {
+        var missingValueArray = [];
+        var containsAll = true;
+
+        options.forEach(function (e, i, a) {
+            var hasValue = false;
+            arrayOfArrays.forEach(function (e1, i1, a1) {
+                if (e.indexOf(e1) > -1) {
+                    hasValue = true;
+                }
+            });
+
+            if (!hasValue) {
+                missingValueArray = a;
+                containsAll = false;
+            }
+        });
+
+        if (!containsAll) {
+            arrayOfArrays[Math.floor(Math.random() * arrayOfArrays.length)] = missingValueArray.charAt(Math.floor(Math.random() * missingValueArray.length));
+            finalPassword = "";
+            arrayOfArrays.forEach(function (e, i, a) {
+                finalPassword += e;
+            });
+            checkPassword();
+        }
+    }
+ 
+   
+  
+  {console.log(finalPassword);}
+    
